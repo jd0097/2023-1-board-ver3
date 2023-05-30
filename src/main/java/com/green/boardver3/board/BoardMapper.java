f7cb9a923cd1fce0946ba172294d82a548f958dc
진혁재 <jhj02201@gmail.com>
2023-05-30 17:59:32 +0900

    BoardMapper, xml 수정 및 Entity 생성

--- src/main/java/com/green/boardver3/board/BoardMapper.java
+++ src/main/java/com/green/boardver3/board/BoardMapper.java
@@ -1,7 +1,9 @@
 package com.green.boardver3.board;
 
+import com.green.boardver3.board.model.BoardDto;
 import com.green.boardver3.board.model.BoardEntity;
 import com.green.boardver3.board.model.BoardInsDto;
+import com.green.boardver3.board.model.BoardVo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,5 +11,5 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
     public int insBoard(BoardInsDto dto);
-    List<BoardEntity> selBoardAll();
+    List<BoardVo> selBoardAll(BoardDto dto);
 }
