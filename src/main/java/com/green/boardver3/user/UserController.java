5cbc557ce0bdcbe4b080f061c01efd03f19a09af
진혁재 <jhj02201@gmail.com>
2023-05-30 12:16:53 +0900

    User 패키지생성

--- src/main/java/com/green/boardver3/user/UserController.java
+++ src/main/java/com/green/boardver3/user/UserController.java
@@ -1,5 +1,6 @@
 package com.green.boardver3.user;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,12 @@ public class UserController {
         this.service = service;
     }
     @PostMapping
+    @Operation(summary = "회원가입", description = "" +
+            "uid: [20] 회원 아이디, <br>" +
+            "upw : [100] 회원 비밀번호,<br>" +
+            "nm : [30] 회원명,<br>" +
+            "gender: [1] 성별(M: 남성, F:여성),<br>"+
+            "addr: [100] 대구시 달서구")
     public int postUser(@RequestBody UserEntity entity) {
         return service.insUser(entity);
     }
