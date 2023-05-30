197570aeba849c3e9c18f2ccc25a42f9568a63eb
진혁재 <jhj02201@gmail.com>
2023-05-30 17:52:23 +0900

    BoardMapper, xml 생성

--- src/main/java/com/green/boardver3/board/BoardMapper.java
+++ src/main/java/com/green/boardver3/board/BoardMapper.java
@@ -1,9 +1,13 @@
 package com.green.boardver3.board;
 
+import com.green.boardver3.board.model.BoardEntity;
 import com.green.boardver3.board.model.BoardInsDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
     public int insBoard(BoardInsDto dto);
+    List<BoardEntity> selBoardAll();
 }
