7a2587f264456e804af17f9755176f7156b8e5b3
진혁재 <jhj02201@gmail.com>
2023-05-30 16:40:01 +0900

    BoardEntity, InsDto 생성 및 수정

--- src/main/java/com/green/boardver3/board/BoardInsDto.java
+++ src/main/java/com/green/boardver3/board/BoardInsDto.java
@@ -3,11 +3,8 @@ package com.green.boardver3.board;
 import lombok.Data;
 
 @Data
-public class BoardEntity {
-    private int iboard;
+public class BoardInsDto {
     private String title;
     private String ctnt;
     private int iuser;
-    private String createdAt;
-    private String updatedAt;
 }
