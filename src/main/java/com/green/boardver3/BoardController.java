cc3fb72160318c59e0f48c62358781c83de6e507
진혁재 <jhj02201@gmail.com>
2023-05-30 10:55:11 +0900

    Controller, Service Post작업 완료

--- src/main/java/com/green/boardver3/BoardController.java
+++ src/main/java/com/green/boardver3/BoardController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/boardver3")
 public class BoardController {
 
     private final BoardService service;
