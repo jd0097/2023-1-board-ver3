6f23d78c6eb5abfce0c9c8c2db7c778eb170ed4a
진혁재 <jhj02201@gmail.com>
2023-05-30 10:48:36 +0900

    Controller, Service Post작업 완료

--- src/main/java/com/green/boardver3/BoardController.java
+++ src/main/java/com/green/boardver3/BoardController.java
@@ -1,4 +1,25 @@
 package com.green.boardver3;
 
+import com.green.boardver3.model.BoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService service;
+
+    @Autowired
+    public BoardController(BoardService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public int postBoard(@RequestBody BoardDto dto) {
+        return service.insBoard(dto);
+    }
 }
--- src/main/java/com/green/boardver3/BoardService.java
+++ src/main/java/com/green/boardver3/BoardService.java
@@ -1,4 +1,19 @@
 package com.green.boardver3;
 
+import com.green.boardver3.model.BoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+    private final BoardverMapper mapper;
+
+    @Autowired
+    public BoardService(BoardverMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public int insBoard(BoardDto dto) {
+        return mapper.insBoard(dto);
+    }
 }
