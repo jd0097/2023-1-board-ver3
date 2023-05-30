d05fa19225f4c00e92966f8bd29982bea497ea1d
진혁재 <jhj02201@gmail.com>
2023-05-30 11:39:00 +0900

    User 패키지생성

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -1,4 +1,12 @@
 package com.green.boardver3.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserMapper mapper;
+
+    public UserService(UserMapper mapper) {
+        this.mapper = mapper;
+    }
 }
