dc1007c22b94aaaa1874c89e51feb60f2f10cdc7
진혁재 <jhj02201@gmail.com>
2023-05-30 11:03:43 +0900

    Dto수정

--- src/main/java/com/green/boardver3/model/BoardDto.java
+++ src/main/java/com/green/boardver3/model/BoardDto.java
@@ -3,9 +3,5 @@ package com.green.boardver3.model;
 import lombok.Data;
 @Data
 public class BoardDto extends BoardEntity{
-    public int uid;
-    public String upw;
-    public String name;
-    public String gender;
-    public String addr;
+
 }
