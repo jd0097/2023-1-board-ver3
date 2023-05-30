14434bc79b363137e57476391777f92e4f0a683d
성수천 <3598ysm@gmail.com>
2023-05-30 16:37:11 +0900

    aa

--- src/main/java/com/green/boardver3/board/BoardService.java
+++ src/main/java/com/green/boardver3/board/BoardService.java
@@ -1,4 +1,15 @@
 package com.green.boardver3.board;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+    private final BoardMapper mapper;
+
+    public BoardService(BoardMapper mapper) {
+        this.mapper = mapper;
+    }
+    public int insboard(BoardEntity entity){
+        return mapper.insboard
+    }
 }
