3d3c54ef52a8070dcbba56658bdcdb545a1fa1a4
성수천 <3598ysm@gmail.com>
2023-05-31 16:02:01 +0900

    Delete

--- src/main/java/com/green/boardver3/cmt/CmtController.java
+++ src/main/java/com/green/boardver3/cmt/CmtController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/cmt")
 public class CmtController {
     private final CmtService service;
 @Autowired
