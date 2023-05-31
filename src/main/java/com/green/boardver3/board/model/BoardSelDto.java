85b8ddca449fe9fc55207ed11053474d556ae557
성수천 <3598ysm@gmail.com>
2023-05-31 12:13:57 +0900

    Service 처

--- src/main/java/com/green/boardver3/board/model/BoardSelDto.java
+++ src/main/java/com/green/boardver3/board/model/BoardSelDto.java
@@ -1,14 +1,12 @@
 package com.green.boardver3.board.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+
 @Data
 public class BoardSelDto {
-        private int iboard;
-        private String title;
-        private String ctnt;
-        private int iuser;
-        private String createdAt;
-        private String updatedAt;
-        private String iuserMainPic;
-
+    private int startIdx;
+    private int page;
+    private int row;
 }
