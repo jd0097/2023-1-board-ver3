3803b09e9faf21992d4515a881e1873030e15f25
진혁재 <jhj02201@gmail.com>
2023-05-31 11:56:04 +0900

    BoardSelMaxDto 생성

--- src/main/java/com/green/boardver3/board/BoardMapper.java
+++ src/main/java/com/green/boardver3/board/BoardMapper.java
@@ -10,5 +10,5 @@ public interface BoardMapper {
     public int insBoard(BoardInsDto dto);
     List<BoardVo> selBoardAll(BoardDto dto);
     int selMaxBoard(int row);
-    BoardSelDto selBoardById(BoardDto dto);
+    BoardSelDto selBoardById(BoardSelDto dto);
 }
