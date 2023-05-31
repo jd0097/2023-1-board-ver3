1d959ee4efe96fcff2f0128d1f973d5c94c1d79d
진혁재 <jhj02201@gmail.com>
2023-05-31 15:58:01 +0900

    CmtInsDto 생성 및 수정

--- src/main/java/com/green/boardver3/cmt/CmtMapper.java
+++ src/main/java/com/green/boardver3/cmt/CmtMapper.java
@@ -1,7 +1,8 @@
 package com.green.boardver3.cmt;
 
 import com.green.boardver3.cmt.model.CmtEntity;
+import com.green.boardver3.cmt.model.CmtInsDto;
 
 public interface CmtMapper {
-    int insCmt(CmtEntity entity);
+    int insCmt(CmtInsDto dto);
 }
