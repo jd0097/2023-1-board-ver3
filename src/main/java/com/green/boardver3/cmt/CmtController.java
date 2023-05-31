09aa9354f77373a60cb9440f2291d7be873144d3
성수천 <3598ysm@gmail.com>
2023-05-31 17:00:44 +0900

    Delete

--- src/main/java/com/green/boardver3/cmt/CmtController.java
+++ src/main/java/com/green/boardver3/cmt/CmtController.java
@@ -16,12 +16,7 @@ public class CmtController {
         this.service = service;
     }
     @PostMapping
-    public int insCmt(@RequestBody CmtEntity dto){
+    public int insCmt(@RequestBody CmtInsDto dto){
     return service.insCmt(dto);
-    }
-    @GetMapping("/{iboard}")
-    public List getCmt(@PathVariable int iboard){
-
-    return
     }
 }
