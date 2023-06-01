077161ce1900afa43096855819ce327c91e07e14
진혁재 <jhj02201@gmail.com>
2023-06-01 11:38:24 +0900

    CmtInsDto 생성 및 수정

--- src/main/java/com/green/boardver3/cmt/CmtController.java
+++ src/main/java/com/green/boardver3/cmt/CmtController.java
@@ -10,26 +10,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/board/cmt")
+@RequestMapping("/board")
 public class CmtController {
     private final CmtService service;
 @Autowired
     public CmtController(CmtService service) {
         this.service = service;
     }
-    @PostMapping
-    public int insCmt(@RequestBody CmtInsDto dto){
+    @PostMapping("/{iboard}")
+    public int insCmt(@PathVariable int iboard
+                    , @RequestBody CmtInsDto dto){
         return service.insCmt(dto);
     }
     @GetMapping
-    public List<CmtVo> getCmt(@RequestParam ("iboard") int iboard
-                             , @RequestParam ("page") int page
-                             ,@RequestParam ("row") int row) {
+    public List<CmtVo> getCmt( @RequestParam int page
+                             ,@RequestParam (defaultValue = "5") int row) {
         CmtVo vo = new CmtVo();
         CmtSelDto dto = new CmtSelDto();
         dto.setPage(page);
         dto.setRow(row);
-        vo.setIboard(iboard);
         return service.selCmt(dto);
     }
 }
