1a3e9bf2380f562ba22e7a674b441c7ec94eeae9
배성현 <zkti12345@naver.com>
2023-06-01 11:55:21 +0900

    service

--- src/main/java/com/green/boardver3/cmt/CmtController.java
+++ src/main/java/com/green/boardver3/cmt/CmtController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/board/cmt")
 public class CmtController {
     private final CmtService service;
 @Autowired
